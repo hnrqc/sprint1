96c72802078e9a6d3eb749b3a407d903f928907d
Henrique Chagas <henrique.chagas@aluno.ufop.edu.br>
2022-02-03 11:24:48 -0300

    Capitulo 4 Aula 1 sequencia do jogo.

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -1,21 +1,31 @@
 print("BEM VINDO AO JOGO DE ADIVINHAÇÃO")
 
+
 numero_secreto = 42
+total_de_tentativas = 3
+rodada = 1
+while(rodada <=  3):
+    print("Tentativa", rodada, "de 3",  )
+    chute = input("Digite Seu Numero: ")
+    chute = int(chute)
+    print("Você Digitou", chute)
 
-chute = input("Digite Seu Numero: ")
-chute = int(chute)
-print("Você Digitou", chute)
 
-acertou = numero_secreto == chute
-maior   = chute > numero_secreto
-menor   = chute < numero_secreto
-if (acertou):
-    print("Você Acertou!!")
-else:
-    if(maior):
-        print("O seu chute foi pra cima")
-    elif (menor):
-        print("O seu chute foi pra baixo")
+    acertou = numero_secreto == chute
+    maior   = chute > numero_secreto
+    menor   = chute < numero_secreto
+
+
+    if (acertou):
+        print("Você Acertou!!")
+    else:
+        if(maior):
+            print("O seu chute foi pra cima")
+        elif (menor):
+            print("O seu chute foi pra baixo")
+
+    rodada = rodada + 1
+
 
 
 print("Fim Do Jogo.")
