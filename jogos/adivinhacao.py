4b22e9598f5ad52d639de047f886429793daba46
Henrique Chagas <henrique.chagas@aluno.ufop.edu.br>
2022-02-03 10:41:47 -0300

    aula 3 finalizada

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -3,5 +3,10 @@ print("BEM VINDO AO JOGO DE ADIVINHAÇÃO")
 numero_secreto = 42
 
 chute = input("Digite Seu Numero: ")
-
+chute = int(chute)
 print("Você Digitou", chute)
+
+if (numero_secreto == chute):
+    print("Você Acertou!!")
+else:
+    print("Você errou")
