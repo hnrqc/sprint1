2ae16096a5142d7e6682aff49ede48f5d892c74f
Henrique Chagas <henrique.chagas@aluno.ufop.edu.br>
2022-02-04 09:43:50 -0300

    Capitulo 7 Aula 2 Definindo uma pontuação para o usuário

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -4,9 +4,23 @@ print("BEM VINDO AO JOGO DE ADIVINHAÇÃO")
 print("********************************")
 
 numero_secreto = random.randrange(1,101)
-print(numero_secreto)
-total_de_tentativas = 3
+total_de_tentativas = 0
+pontos = 1000
 
+#print(numero_secreto)
+
+
+
+print("Qual nível de dificuldade?")
+print("(1) Fácil (2) Médio (3) Difícil")
+nivel =  int (input("Defina o nível: "))
+
+if (nivel == 1):
+    total_de_tentativas = 20
+elif (nivel == 2):
+    total_de_tentativas = 10
+else:
+    total_de_tentativas = 5
 
 for rodada in range(1, total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
@@ -24,13 +38,24 @@ for rodada in range(1, total_de_tentativas + 1):
 
 
     if (acertou):
-        print("Você Acertou!!")
+        print("Você Acertou e fez {} pontos!!".format(pontos))
         break
     else:
         if(maior):
             print("O seu chute foi pra cima")
+            pontos_perdidos = abs(numero_secreto - chute)
+            pontos = pontos - pontos_perdidos
+
+            # print("{}".format(pontos_perdidos))
+            #print("{}".format(pontos))
+
         elif (menor):
             print("O seu chute foi pra baixo")
+            pontos_perdidos = abs(numero_secreto - chute)
+            pontos = pontos - pontos_perdidos
+
+            # print("{}".format(pontos_perdidos))
+            #print("{}".format(pontos))
 
     rodada = rodada + 1
 
